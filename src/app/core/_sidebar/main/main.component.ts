b41e672fe9631b1d94578fc111cc92dfa337d913
Michael <michael@codetropolis.com>
2020-03-27 08:20:09 -0500

    Change nav bar icon based on active route.

--- src/app/core/_sidebar/main/main.component.ts
+++ src/app/core/_sidebar/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLinkActive, ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
@@ -8,13 +9,36 @@ import { Component, OnInit } from '@angular/core';
 export class MainComponent implements OnInit {
 value: string;
 
-  constructor() { }
+isActive: boolean[] = [];
+// isActive: boolean;
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+
+    this.router.events.subscribe((val) => {
+      // see also 
+      console.log(val); 
+  });
+
+    // this.route.params.subscribe(params => {
+    //   console.log(`MD: MainComponent -> ngOnInit -> params`, params);
+    //   if(params['slug']){
+    //    this.isActive = true;
+    //   }else{
+    //    this.isActive = false;
+    //   }
+    // });
   }
 
   clickFunction = () => {
     console.log('clicked');
     this.value = 'Clicked';
   }
+
+  highlight(item: string) {
+    this.isActive[item] = !this.isActive[item];
+    console.log(`MD: MainComponent -> highlight -> this.isActive[item]`, this.isActive[item]);
+  }
+
 }
